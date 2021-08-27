b2285b1d78f811744dd8635d842b4be48435b0fa
Denis Kozhevnikov <newlife_kod@mail.ru>
2021-08-27 15:39:13 +0300

    focus-universal added, css fixed, burger menu fixed, search fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var mediaQuery = window.matchMedia("screen and (max-width: 600px)");
+  mediaQuery.addListener(myFunc);
+
+  function myFunc(mq) {
+      if (mq.matches) {
+        document.querySelectorAll('.events__card').forEach(function(event) {
+          event.classList.add('events__card-active')
+        });
+        document.querySelector('.events__container').classList.add('event__swiper');
+        document.querySelector('.events__list').classList.add('swiper-wrapper');
+        document.querySelectorAll('.events__card').forEach(function(addSwiper) {
+          addSwiper.classList.add('swiper-slide');
+        });
+        document.querySelector('.books__swiper-mobile').classList.remove('books__swiper');
+        document.querySelector('.books__swiper-wrapper-mobile').classList.remove('swiper-wrapper');
+        document.querySelectorAll('.books__card').forEach(function(removeSwiper) {
+          removeSwiper.classList.remove('swiper-slide');
+        })
+      }
+
+      else {
+        document.querySelectorAll('.events__card').forEach(function(event) {
+          event.classList.remove('events__card-active')
+        });
+        document.querySelector('.events__container').classList.remove('event__swiper');
+        document.querySelector('.events__list').classList.remove('swiper-wrapper');
+        document.querySelectorAll('.events__card').forEach(function(addSwiper) {
+          addSwiper.classList.remove('swiper-slide');
+        })
+        document.querySelector('.books__swiper-mobile').classList.add('books__swiper');
+        document.querySelector('.books__swiper-wrapper-mobile').classList.add('swiper-wrapper');
+        document.querySelectorAll('.books__card').forEach(function(removeSwiper) {
+          removeSwiper.classList.add('swiper-slide');
+        })
+      }
+    };
+  myFunc(mediaQuery);
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.add('is-active')
   });
@@ -172,7 +209,7 @@ document.addEventListener('DOMContentLoaded', function() {
           spaceBetween: 20,
           initialSlide: 2,
         },
-        // when window width is >= 768px
+        // when window width is >= 601px
         601: {
           slidesPerColumn: 2,
           slidesPerView: 2,
@@ -210,8 +247,8 @@ document.addEventListener('DOMContentLoaded', function() {
           slidesPerGroup: 1,
           spaceBetween: 34,
         },
-        // when window width is >= 992px
-        992: {
+        // when window width is >= 993px
+        993: {
           slidesPerView: 2,
           slidesPerGroup: 1,
           spaceBetween: 50,
@@ -252,8 +289,8 @@ document.addEventListener('DOMContentLoaded', function() {
           slidesPerGroup: 2,
           spaceBetween: 34,
         },
-        // when window width is >= 640px
-        992: {
+        // when window width is >= 993px
+        993: {
           slidesPerView: 2,
           slidesPerGroup: 2,
           spaceBetween: 50,
