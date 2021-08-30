85d3342d935f664476d2fb890c3c35f7b400af3d
Denis Kozhevnikov <newlife_kod@mail.ru>
2021-08-30 16:03:25 +0300

    mail.php added

--- js/main.js
+++ js/main.js
@@ -424,6 +424,24 @@ document.addEventListener('DOMContentLoaded', function() {
           }
         },
       },
+      submitHandler: function(form) {
+        let formData = new FormData(form);
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              console.log('Отправлено');
+            }
+          }
+        }
+
+        xhr.open('POST', 'mail.php', true);
+        xhr.send(formData);
+
+        form.reset();
+      },
       messages: {
         name: {
           required: 'Заполните поле!',
